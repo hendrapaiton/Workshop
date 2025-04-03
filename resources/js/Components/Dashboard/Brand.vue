d8823e5f088086138d87dbf67ff0fd4ed81852d1
Hendra Dwi Saputra <hendra@usa.com>
2025-04-03 21:21:45 +0700

    Add Brand component to Index and implement navigation

---
+++ resources/js/Components/Dashboard/Brand.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg-white w-full">
+        <nav class="bg-blue-700 p-4 mx-auto xl:w-3/4 xl:mt-4 lg:w-3/4 lg:mt-4 md:w-3/4 md:mt-4">
+            <div class="container mx-auto flex items-center justify-between">
+                <div class="flex items-center space-x-4">
+                    <img alt="Company Logo" class="h-10 w-10" height="40"
+                        src="https://storage.googleapis.com/a1aa/image/eIjNI_w5oUclIFwdESw3hHsT6DnZHzav6yYuOQiGU-E.jpg"
+                        width="40" />
+                    <span class="text-white text-xl font-semibold">eCommerce</span>
+                </div>
+            </div>
+            <div class="container mx-auto mt-4 hidden md:block lg:block xl:block text-white" id="mobile-menu">
+                <div class="w-full flex justify-center space-x-3">
+                    <a class="hover:underline border-b-2 border-white" href="#">Dashboard</a>
+                    <a class="hover:underline border-white" href="#">Project</a>
+                    <a class="hover:underline border-white" href="#">Order</a>
+                    <a class="hover:underline border-white" href="#">Staff</a>
+                    <a class="hover:underline border-white" href="#">Customer</a>
+                    <a class="hover:underline border-white" href="#">Setting</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
--- resources/js/Components/Index.vue
+++ resources/js/Components/Index.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import Brand from './Dashboard/Brand.vue';
 import Copyright from './Dashboard/Copyright.vue';
 
 </script>
 
 <template>
+    <Brand />
     <Copyright />
 </template>
