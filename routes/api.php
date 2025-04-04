ca2ccb219a2d7371be88e262a83f29262cbda35f
Hendra Dwi Saputra <hendra@usa.com>
2025-04-04 14:01:28 +0700

    Add ProjectController and API routes for project management

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\ProjectController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('project')->group(function () {
+    Route::get('/', [ProjectController::class, 'index']);
+    Route::post('/', [ProjectController::class, 'store']);
+    Route::get('/{id}', [ProjectController::class, 'show']);
+    Route::put('/{id}', [ProjectController::class, 'update']);
+    Route::delete('/{id}', [ProjectController::class, 'destroy']);
+});
