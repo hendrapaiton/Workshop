bdc667c6cf6b4c6c67c1546552fcaa028dcc385d
Hendra Dwi Saputra <hendra@usa.com>
2025-04-04 08:57:02 +0700

    Add Login component and update Index to include it

--- resources/js/Components/Index.vue
+++ resources/js/Components/Index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Brand from './Layout/Brand.vue';
 import Copyright from './Layout/Copyright.vue';
+import Login from './Login/Index.vue';
 import Dashboard from './Dashboard/Index.vue';
 
 </script>
 
 <template>
-    <Brand />
-    <Dashboard />
-    <Copyright />
+    <!-- <Brand /> -->
+    <!-- <Dashboard /> -->
+    <Login />
+    <!-- <Copyright /> -->
 </template>
